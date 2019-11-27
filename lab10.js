cb6889a8e4403f41f8d8563344b0846b715cc389
Priyanka Khadilkar <49756361+Priyanka-Khadilkar@users.noreply.github.com>
2019-11-27 11:23:34 -0500

    Add files via upload

---
+++ lab10.js
@@ -0,0 +1,35 @@
+﻿//LAB 10-DATA STORAGE: INDEX PAGE
+function init(){
+	
+	 var txtName = document.getElementById("inName");
+	 var colorVal = document.getElementById("inColor");
+	 var infoForm = document.getElementsByTagName("form")[0];
+	 var heading = document.getElementById("newMsgBox");
+	 var btnDel = document.getElementById("btnDel");
+	
+	
+	 txtName.value = localStorage.getItem("name");
+	 if(localStorage.getItem("name") !== null)
+	 {
+	  heading.innerHTML = "Welcome "  + localStorage.getItem("name");
+	 }
+	 document.body.style.backgroundColor = localStorage.getItem("backgroundcolor");
+	 colorVal.value = localStorage.getItem("backgroundcolor");
+	
+	infoForm.onsubmit = function (){
+	   localStorage.setItem("backgroundcolor",colorVal.value);
+	   localStorage.setItem ("name",txtName.value);  
+       txtName.value = localStorage.getItem("name");
+	   colorVal.value = localStorage.getItem("backgroundcolor");	   
+    }
+	
+	btnDel.onclick = function ()
+	{
+		localStorage.clear();
+		location.reload();
+	}
+
+}
+
+window.onload = init;
+
---
+++ products.js
@@ -0,0 +1,19 @@
+﻿//LAB 10-DATA STORAGE: PRODUCTS PAGE
+//alert("products");//DELETE AFTER CONFIRMING CONNECTION
+
+//LAB 10-DATA STORAGE: INDEX PAGE
+function init(){
+	
+	 var txtName = document.getElementById("newMsgBox");
+	
+	 if(localStorage.getItem("name") !== null)
+	 {
+	  txtName.innerHTML = "Welcome "  + localStorage.getItem("name");
+	 }
+	 document.body.style.backgroundColor = localStorage.getItem("backgroundcolor");
+	
+
+}
+
+window.onload = init;
+
